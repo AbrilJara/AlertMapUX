0c9d6fe901fce6aeb2381507b5972a1fe7547873
Abril Jara <rodrigojara1997@gmail.com>
2022-06-27 12:09:13 -0300

    Agregado de cookies y al layout la lógica necesaria para cargar la configuración de acuerdo a estas cookies

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from operator import invert
 from flask import Flask
 from flask import request
+from flask import make_response
 from flask import render_template
 from flask import abort, redirect, url_for
 
@@ -7,7 +9,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+
+    if 'primera_vez' in request.cookies:
+        return render_template('index.html')
+    else:
+        response = make_response(render_template('index.html'))
+        response.set_cookie('primera_vez', 'true')
+        response.set_cookie('invert_color', 'false')
+        response.set_cookie('tamanio_texto', '0')
+        return response
+
+    
 
 @app.route("/configuracion")
 def configuracion():
